3a13988ffc019a2750eb506850195731982dc884
Ankur Raj <133074916+ankur0999@users.noreply.github.com>
2024-02-13 16:10:56 +0530

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -18,10 +18,20 @@ class complex{
              this->x = c.x;
              this->y = c.y;
              }
+	 complex(int x){
+		 this->x = x;
+		 this->y = 0;
+	 }
         complex operator +(complex c){
             complex tmp;
                 tmp.x = this->x + c.x;
                 tmp.y = this->y + c.y;
+                return tmp;
+                }
+        complex operator +(int x){
+            complex tmp;
+                tmp.x = this->x + x;
+                tmp.y = this->y + 0;
                 return tmp;
                 }
         complex operator *(complex c){
@@ -30,18 +40,27 @@ class complex{
              tmp.y = this->x*c.y + this->y*c.x;
              return tmp;
              }
+        complex operator *(int x){
+             complex tmp;
+             tmp.x = this->x*x - this->y*0;
+             tmp.y = this->x*0 + this->y*x;
+             return tmp;
+             }
         void show_data(){
             cout<<x<<" "<<y<<endl;
             }
             };
-
+            
 int main(){
-    complex c1(1,2), c2(2,3), c3,c4;
-
-    c3 = c1+c2 ;
+    complex c1(1,2), c2(2,3), c3,c4, c6(1);
+    
+    c3 = c1+c2+2;
+    //c3.show_data();
+    //c4 = c3*c2;
+    //complex c5(c4);
+    //c5.show_data();
+    //c6 = c1+c2*c4;
     c3.show_data();
-    c4 = c3*c2;
-    complex c5(c4);
-    c5.show_data();
-
+    
     }
+    
